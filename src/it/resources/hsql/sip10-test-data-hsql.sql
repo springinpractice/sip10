b66fd5933c7b6824bb778f1a88c2268a444e9917
Willie Wheeler <willie.wheeler@gmail.com>
2011-11-03 02:01:00 -0700

    support for embedded database

---
+++ src/it/resources/hsql/sip10-test-data-hsql.sql
@@ -0,0 +1,19 @@
+-- ============================================================================
+-- $Id: sip10-dml.sql 36 2010-05-05 06:48:23Z  $
+-- 
+-- Copyright (c) 2010 Manning Publications Co.
+-- 
+-- Willie Wheeler (willie.wheeler@gmail.com)
+-- 
+-- Book web site   - http://www.manning.com/wheeler/
+-- Book blog       - http://springinpractice.com/
+-- Author web site - http://wheelersoftware.com/
+-- ============================================================================
+
+insert into contact values (1, 'Zimmerman', 'Robert', 'A', 'bobdylan@example.com', null, null);
+insert into contact values (2, 'Osbourne', 'John', 'M', 'ozzyosbourne@example.com', null, null);
+insert into contact values (3, 'Mapother', 'Tom', 'C', 'tomcruise@example.com', null, null);
+insert into contact values (4, 'Norris', 'Carlos', 'R', 'chucknorris@example.com', null, null);
+insert into contact values (5, 'Johnson', 'Caryn', 'E', 'whoppigoldberg@example.com', null, null);
+insert into contact values (6, 'Smith', 'John', null, 'johnsmith@example.com', null, null);
+insert into contact values (7, 'Smith', 'Jane', 'X', null, null, null);

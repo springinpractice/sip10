25343a08f7cbc9390e25014b2b2664d598c52085
Willie Wheeler <willie.wheeler@gmail.com>
2013-04-29 23:52:48 -0700

    Upgraded beans schema to Spring 3.1. Updated legal/authorship boilerplate.

--- src/main/java/com/springinpractice/ch10/dao/HbnContactDao.java
+++ src/main/java/com/springinpractice/ch10/dao/HbnContactDao.java
@@ -1,11 +1,10 @@
 /* 
- * $Id: HbnContactDao.java 20 2010-05-02 08:36:22Z  $
+ * Copyright (c) 2013 Manning Publications Co.
  * 
- * Copyright (c) 2010 Manning Publications Co.
- * 
- * Book web site   - http://www.manning.com/wheeler/
- * Book blog       - http://springinpractice.com/
- * Author web site - http://wheelersoftware.com/
+ * Blog   : http://springinpractice.com/
+ * GitHub : https://github.com/organizations/springinpractice
+ * Book   : http://manning.com/wheeler/
+ * Forum  : http://www.manning-sandbox.com/forum.jspa?forumID=503
  */
 package com.springinpractice.ch10.dao;
 
@@ -19,7 +18,6 @@ import com.springinpractice.dao.hibernate.AbstractHbnDao;
  * Hibernate-backed data access object implementation for contacts.
  * </p>
  * 
- * @version $Id: HbnContactDao.java 20 2010-05-02 08:36:22Z  $
  * @author Willie Wheeler (willie.wheeler@gmail.com)
  */
 @Repository
